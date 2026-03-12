56e9ce672c44ea10c115bf1f87d573aea473ef80
Matteo MATHIS <mathis136u@etu.univ-lorraine.fr>
2026-03-12 15:20:33 +0100

    debut

---
+++ Appli_suivie_serie_film/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Appli_suivie_serie_film',
+  webDir: 'www'
+};
+
+export default config;
