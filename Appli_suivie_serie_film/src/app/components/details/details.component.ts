d4bc5214b550face20dad7064cb2e54e5ba9d20f
Matteo MATHIS <mathis136u@etu.univ-lorraine.fr>
2026-03-20 22:07:19 +0100

    detail

---
+++ Appli_suivie_serie_film/src/app/components/details/details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss'],
+  standalone: false,
+})
+export class DetailsComponent   {
+
+  constructor() { }
+
+
+
+}
